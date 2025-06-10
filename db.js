3398d7d0234cad2a43ee9f40a69089e3cce6a8b8
LUIS OTAVIO ROSA ROCHA TANESE <luis_tanese@estudante.sesisenai.org.br>
2025-06-10 08:59:20 -0300

    Update Patch

--- db.js
+++ db.js
@@ -1,68 +1,63 @@
-async function connect() {  
-    const { Pool } = require("pg");
+const connect = async () => {
+	const { Pool } = require("pg");
 
-    if(global.connection)
-        return global.connection.connect();
+	if (global.connection) return global.connection.connect();
 
-    const pool = new Pool({
-      user: process.env.USER_NAME,
-      host: process.env.HOST_NAME,
-      database: process.env.DB_NAME,
-      password: process.env.DB_PASSWORD,
-      dialect: process.env.DB_DIALECT,
-      port: process.env.PORT_NUMBER
-    });
-    
-    const client = await pool.connect();
-    console.log("O Pool de conexão foi criado com sucesso!")
-    client.release();
+	const pool = new Pool({
+		user: process.env.USER_NAME,
+		host: process.env.HOST_NAME,
+		database: process.env.DB_NAME,
+		password: process.env.DB_PASSWORD,
+		dialect: process.env.DB_DIALECT,
+		port: process.env.PORT_NUMBER,
+	});
 
-    global.connection = pool;
-    
-    return pool.connect();
-  }
+	const client = await pool.connect();
+	console.log("O Pool de conexão foi criado com sucesso!");
+	client.release();
 
-  connect();
+	global.connection = pool;
 
+	return pool.connect();
+};
 
-// Função para listar clientes
-async function selectCustomers() {
-  // Estabelecer conexão com o banco de dados
-  const client = await connect();
-  // Enviar comando SQL para o banco de dados
-  const res = await client.query("SELECT * FROM client");
-  // Retorna as linhas (registros) da tabela
-  return res.rows;
-  }
+connect();
 
-  // Função para listar um cliente
-async function selectCustomer(id) {
-  // Estabelece a conexão com o banco de dados
-  const client = await connect();
-  // Executa a query SQL usando declaração preparada para evitar SQL Injection
-  const res = await client.query("SELECT * FROM client WHERE cpf=$1", [id]);
-  // Retorna as linhas (dados do cliente)
-  return res.rows;
-  }
+const selectCustomers = async () => {
+	const client = await connect();
+	const res = await client.query("SELECT * FROM client");
+	return res.rows;
+};
 
-  // Função para inserir clientes
-async function insertCustomers(customers) {
+const selectCustomer = async (id) => {
+	const client = await connect();
+	const res = await client.query("SELECT * FROM client WHERE cpf=$1", [id]);
+	return res.rows;
+};
 
-  // Estabelecer conexão
-  const client = await connect();
+const insertCustomers = async (customers) => {
+	const client = await connect();
+	const sql = "INSERT INTO client (cpf, nome, email, idade, profissao) VALUES ($1, $2, $3, $4, $5);";
+	const values = [customers.cpf, customers.nome, customers.email, customers.idade, customers.profissao];
+	await client.query(sql, values);
+};
 
-  // query
-  const sql = "INSERT INTO client(cpf, nome, email, idade, profissao) VALUES($1, $2, $3, $4, $5); "
+const deleteCustomer = async (id) => {
+	const client = await connect();
+	await client.query("DELETE FROM client WHERE cpf=$1", [id]);
+};
 
-  //parametros injetados na consulta
-  const values = [customers.cpf, customers.nome, customers.email, customers.idade, customers.profissao];
+const updateCustomer = async (id, customer) => {
+	const client = await connect();
+	const sql = "UPDATE client SET nome=$1, email=$2, idade=$3, profissao=$4 WHERE cpf=$5";
+	const values = [customer.nome, customer.email, customer.idade, customer.profissao, id];
+	await client.query(sql, values);
+};
 
-  await client.query(sql, values);
-
-}
-
-  module.exports = {
-    insertCustomers,
-    selectCustomers,
-    selectCustomer
-  }
+module.exports = {
+	insertCustomers,
+	selectCustomers,
+	selectCustomer,
+	deleteCustomer,
+	updateCustomer,
+};
--- index.js
+++ index.js
@@ -1,40 +1,36 @@
-require("dotenv").config(); 
+require("dotenv").config();
 
 const db = require("./db");
-
 const port = process.env.PORT;
-
-const express = require('express');
-
-const app = express();
+const app = require("express");
 
 app.use(express.json());
 
+app.get("/client", async (req, res) => {
+	const clientes = await db.selectCustomers();
+	res.json(clientes);
+});
 
-// Rota para listar todos os clientes
-app.get('/client', async (req, res) => {
-    // Chama a função que seleciona os clientes no banco de dados
-    const clientes = await db.selectCustomers();
-    // Envia a resposta em formato JSON contendo os clientes
-    res.json(clientes);
-    });
+app.get("/client/:id", async (req, res) => {
+	const cliente = await db.selectCustomer(req.params.id);
+	res.json(cliente);
+});
 
-    // Rota para listar um cliente específico
-app.get('/client/:id', async (req, res) => {
-    // Captura o parâmetro 'id' presente na URL, que corresponde ao CPF do cliente
-    const cliente = await db.selectCustomer(req.params.id);
-    // Responde com os dados do cliente em formato JSON
-    res.json(cliente);
-    });
-
-// Rota para inserir clientes
 app.post("/client", async (req, res) => {
-    await db.insertCustomers(req.body);
+	await db.insertCustomers(req.body);
+	res.sendStatus(201);
+});
 
-    res.sendStatus(201);
-})
+app.patch("/client/:id", async (req, res) => {
+	await db.updateCustomer(req.params.id, req.body);
+	res.sendStatus(200);
+});
 
+app.delete("/client/:id", async (req, res) => {
+	await db.deleteCustomer(req.params.id);
+	res.sendStatus(200);
+});
 
-app.listen(port);
-
-console.log("Backend Rodando!")
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`);
+});
